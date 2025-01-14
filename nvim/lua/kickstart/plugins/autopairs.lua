1e093bbb798a896d7f171eadf847b92701cb931d
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-01-14 23:46:21 +1100

    refactor: prevent autopairs when importing react component

--- nvim/lua/kickstart/plugins/autopairs.lua
+++ nvim/lua/kickstart/plugins/autopairs.lua
@@ -11,6 +11,17 @@ return {
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    cmp.event:on('confirm_done', function(event)
+      local entry = event.entry
+      local word = entry:get_insert_text()
+
+      -- Check if the completion is for a React component
+      local is_react_component = word:match '^[A-Z]' -- React components typically start with an uppercase letter
+
+      -- Call the default behavior only if not a React component
+      if not is_react_component then
+        cmp_autopairs.on_confirm_done()(event)
+      end
+    end)
   end,
 }
