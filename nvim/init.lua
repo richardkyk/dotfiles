e6fcd473a6d68cad40c1b2865ffd9dcd27afb935
Richard Khaw <28803078+richardkyk@users.noreply.github.com>
2026-02-19 10:39:43 +1100

    refactor: use vtsls instead of ts_ls for better import snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -793,7 +793,8 @@ require('lazy').setup({
           --    https://github.com/pmizio/typescript-tools.nvim
           --
           -- But for many setups, the LSP (`ts_ls`) will work just fine
-          ts_ls = {},
+          -- ts_ls = {},
+          vtsls = {},
           tailwindcss = {
             filetypes = { 'html', 'css', 'javascript', 'typescript', 'vue', 'react', 'htmlangular', 'typescriptreact', 'javascriptreact' },
           },
